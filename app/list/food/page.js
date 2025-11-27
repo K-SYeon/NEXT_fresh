42ea094d7eee2458442af56b26f93a2c4e7e1b34
K-SYeon <thdus0539@naver.com>
2025-11-27 18:19:45 +0900

    Add files via upload

---
+++ app/list/food/page.js
@@ -0,0 +1,7 @@
+export default function Food() {
+    return (
+        <div>
+            <h4 className="title">푸드</h4>
+        </div>
+    );
+}
---
+++ app/page.js
@@ -0,0 +1,12 @@
+export default function Home() {
+    let name = 'kim';
+    return (
+        <div>
+            
+            <h4 className="title">
+            <span>에브리</span>후레시
+            </h4>
+            <p className="title-sub">by SY { name }</p>
+        </div>
+    );
+}
